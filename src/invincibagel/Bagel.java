6c3a04de3278a9d0ee03779f8b265cb455d6f53d
Vadzim <vadzimk@hotmail.com>
2019-12-04 16:30:30 -0800

    animated run sycle

--- src/invincibagel/Bagel.java
+++ src/invincibagel/Bagel.java
@@ -7,12 +7,13 @@ import static invincibagel.InvinciBagel.WIDTH;
 
 public class Bagel extends Hero {
     protected InvinciBagel invinciBagel; //holds a reference to the current state of the InvinciBagel game. Protected access allows any subclass of Bagel to access this field
-    protected static final double SPRITE_PIXELS_X = 81/2;
+    protected static final double SPRITE_PIXELS_X = 81;
     protected static final double SPRITE_PIXELS_Y = 81;
-    protected static final double rightBoundary = WIDTH/2 - SPRITE_PIXELS_X / 2;
-    protected static final double leftBoundary = -(WIDTH/2 - SPRITE_PIXELS_X / 2);
-    protected static final double bottomBoundary = HEIGHT/2 - SPRITE_PIXELS_Y / 2;
-    protected static final double topBoundary = -(HEIGHT/2 - SPRITE_PIXELS_Y / 2);
+    protected static final double rightBoundary = WIDTH / 2 - SPRITE_PIXELS_X / 2;
+    protected static final double leftBoundary = -(WIDTH / 2 - SPRITE_PIXELS_X / 2);
+    protected static final double bottomBoundary = HEIGHT / 2 - SPRITE_PIXELS_Y / 2;
+    protected static final double topBoundary = -(HEIGHT / 2 - SPRITE_PIXELS_Y / 2);
+    private boolean animator = false; // a flag used to alternate between imageView(1) - if false and imageView(2) - if true.
 
     public Bagel(InvinciBagel iBagel, String SVGdata, double xLocation, double yLocation, Image... spriteCels) {
         super(SVGdata, xLocation, yLocation, spriteCels);
@@ -24,6 +25,7 @@ public class Bagel extends Hero {
         //will be called on the object in the handle method of the GamePlayloop of AnimationTimer Class
         setXYLocation();
         setBoundaries();
+        setImageState();
         moveInvinciBagel(iX, iY);
     }
 
@@ -70,6 +72,57 @@ public class Bagel extends Hero {
         }
     }
 
+    /**
+     * Choose the image for the ImageView of the sprite according to the keybuttons pressed
+     */
+    private void setImageState() {
+        // Wait state:
+        if (
+                !invinciBagel.isRight() &&
+                        !invinciBagel.isLeft() &&
+                        !invinciBagel.isDown() &&
+                        !invinciBagel.isUp()) {
+            spriteFrame.setImage(imageStates.get(0));
+            animator = false;
+        }
+
+        // Run state:
+        if (invinciBagel.isRight()) {
+
+            spriteFrame.setScaleX(1); // no mirroring the imageView
+            if (!animator) {
+                spriteFrame.setImage(imageStates.get(1));
+                animator = true;
+            } else if (animator) {
+                spriteFrame.setImage(imageStates.get(2));
+                animator = false;
+            }
+        }
+        if (invinciBagel.isLeft()) {
+
+            spriteFrame.setScaleX(-1); // mirror the imageView around Y axis
+            if (!animator) {
+                spriteFrame.setImage(imageStates.get(1));
+                animator = true;
+            } else if (animator) {
+                spriteFrame.setImage(imageStates.get(2));
+                animator = false;
+            }
+        }
+
+        // Jump state:
+        if (invinciBagel.isDown()) {
+            spriteFrame.setImage(imageStates.get(3));
+        }
+        if (invinciBagel.isUp()) {
+            spriteFrame.setImage(imageStates.get(4));
+        }
+
+    }
+
+    /**
+     * relocate the ImageView object of the sprite
+     */
     private void moveInvinciBagel(double x, double y) {
         spriteFrame.setTranslateX(x);
         spriteFrame.setTranslateY(y);
